7046569603aaa3d190fb814eca82c0df3517c9d2
Michael Forbes <michaelforbez@hotmail.co.uk>
2019-04-03 16:58:02 +0100

    Optional Done

---
+++ start_point/my_functions.rb
@@ -0,0 +1,23 @@
+def add_array_lengths(array1, array2)
+  total_length = array1.length + array2.length
+  return total_length
+end
+
+def sum_array(numbers)
+  total = numbers.sum
+  return total
+end
+
+def find_item(array, item)
+  for name in array
+    if (name == item)
+      return true
+    end
+  end
+  return false
+end
+
+def get_first_key(hash)
+  new_array = hash.keys
+  return new_array[0] 
+end
